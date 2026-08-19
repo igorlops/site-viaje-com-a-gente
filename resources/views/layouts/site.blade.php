210937e219be249e41dc95f693b615a3ede12563
Igor Lopes <cifernandes@sfiec.org.br>
2026-08-19 20:12:44 -0300

    feat: integrate Google Search Console package and configure search service layout

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="WKsXn1c8XduEpDJssqU9T_qD7ErNd26FQmW8EwRuAmQ" />
     @if($banner?->page)
         <meta name="title" content="{{ $banner?->page?->meta_title ?? 'Viaje com a Gente - Viagens e Turismo' }}">
         <meta name="description" content="{{ $banner?->page?->meta_description ?? 'Viaje com a Gente - Viagens e Turismo' }}">
